53254a2a6a91a5458e2212a0ff30b553403ae569
Zorrys9 <saschek2000@yandex.ru>
2020-01-26 01:41:51 +0300

    create provider configuration

---
+++ CreateProviderConfiguration/TextConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateProviderConfiguration
+{
+    public static class TextConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddTextFile(this IConfigurationBuilder builder, string path)
+        {
+            if(builder == null)
+            {
+               new ArgumentNullException(nameof(builder));
+            }
+            if (string.IsNullOrEmpty(path))
+            {
+                new Exception("Путь к файлу не задан, либо файла не существует");
+            }
+
+            var source = new TextConfigurationSource(path);
+            builder.Add(source);
+            return builder;
+
+        }
+    }
+}
---
+++ CreateProviderConfiguration/TextConfigurationSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateProviderConfiguration
+{
+    public class TextConfigurationSource : IConfigurationSource
+    {
+        public string FilePath { get; set; }
+        public TextConfigurationSource(string filename)
+        {
+            FilePath = filename;
+        }
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            string filepath = builder.GetFileProvider().GetFileInfo(FilePath).PhysicalPath;
+            return new TextConfigurationProvider(filepath);
+        }  
+    }
+}
